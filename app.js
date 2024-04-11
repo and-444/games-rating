ce401037089ceed308ed3b11d98d0594ed4c268e
and-444 <andreysavinx@yandex.ru>
2024-04-12 00:51:25 +0300

    finish task

---
+++ app.js
@@ -0,0 +1,30 @@
+const {
+  defaultRouteController,
+  mainRouteController,
+  gameRouteController,
+  voteRouteController,
+} = require("./controllers");
+const path = require("path");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  switch (url) {
+    case "/":
+      mainRouteController(res, "/index.html", ".html");
+      break;
+
+    case "/game":
+      gameRouteController(res);
+      break;
+
+    case "/vote":
+      voteRouteController(req, res);
+      break;
+
+    default:
+      defaultRouteController(res, url);
+  }
+});
+
+server.listen(3005);
